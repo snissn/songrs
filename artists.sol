d23deae8fa1cf556a72d5efd7a2e74b599568d9a
Michael Seiler <michaelseiler@Michaels-MBP.home>
2017-08-06 02:58:24 -0400

    blah

--- artists.sol
+++ artists.sol
@@ -1,18 +1,33 @@
 pragma solidity ^0.4.14;
 
 contract Song{
-    
+
     string ipfs;
     string name;
-    function Song(string _ipfs, string _name){
+    address artist;
+    function Song(string _ipfs, string _name, address _artist){
         ipfs = _ipfs;
         name = _name;
-    
+        artist = _artist;
+    }
+}
+
+contract Album{
+
+    Song[] public songs;
+    string public name;
+    address artist;
+    function Album(string _name){
+        name = _name;
     }
 }
 contract Artists {
-    
-    
+
+    event NewArtistEvent(bytes32 username, string ipfs);
+    event NewSongEvent(bytes32 artist_username, string artist_ipfs, string song_name, string song_ipfs);
+    event NewAlbumEvent(bytes32 artist_username, string artist_ipfs, string album_name);
+
+
     struct Artist {
         string ipfs;
         bytes32 name;   // short name (up to 32 bytes)
@@ -23,21 +38,36 @@ contract Artists {
     mapping(address => Artist) public artists;
     mapping(bytes32 => bool) public reserved_names;
 
-    function set(bytes32 username, string ipfs) {
+    function create_artist(bytes32 username, string ipfs) {
        address[] storage albums;
        address[] storage singles;
        artists[msg.sender] = Artist({ipfs:ipfs, name:username, albums:albums,singles:singles});
        reserved_names[username] = true;
+
+       NewArtistEvent(username, ipfs);
+
     }
-    
-    function create_single(string ipfs, string name){
+
+    function create_album(string name) returns (address){
+        address album = address(new Album(name));
+        artists[msg.sender].albums.push(album);
+        Artist artist = artists[msg.sender];
+        NewAlbumEvent(artist.name, artist.ipfs, name);
+        return album;
+    }
+
+    function create_single(string name, string ipfs)  returns (address){
         //songs_mapping[msg.sender].push(Song({ipfs:ipfs, name:name}));
-        address s = address(new Song(ipfs,name));
-        artists[msg.sender].singles.push(s);
-        
+        address song = address(new Song(ipfs,name, msg.sender));
+        artists[msg.sender].singles.push(song);
+        Artist artist = artists[msg.sender];
+        NewSongEvent(artist.name, artist.ipfs, name, ipfs);
+        return song;
+
     }
 
     function get_ipfs(bytes32 name ) constant returns ( string) {
         return artists[msg.sender].ipfs;
     }
 }
+
